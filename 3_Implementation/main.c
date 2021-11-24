c345381e7c3dc5d4ec18f500f5d7099460c52043
Yanadi Prudhvi <94187482+YanadiPrudhvi@users.noreply.github.com>
2021-11-25 00:07:01 +0530

    adding main and make files

---
+++ 3_Implementation/main.c
@@ -0,0 +1,23 @@
+/**
+ * @file main.c
+ * @author Yanadi Prudhvi
+ * @brief main function to run program
+ * @version 0.1
+ * @date 2021-11-24
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include<stdio.h>
+#include "functions.h"
+void main()
+{
+    char* name[50];
+    printf("please enter your name:");
+    gets(name);
+    welcome();
+    rules(name);
+    question1(name);
+    //end();
+    return 0;
+}
