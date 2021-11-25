47234f550b65bc3f646d3d87c7eff558ce91745e
Yanadi Prudhvi <94187482+YanadiPrudhvi@users.noreply.github.com>
2021-11-25 13:05:12 +0530

    comit

--- 3_Implementation/src/question9.c
+++ 3_Implementation/src/question9.c
@@ -55,30 +55,6 @@ question9()
                  goto label2;
              }
          break;
-        case 3:
-          printf("The conspiracy angle of rajiv gadhi murder is being probed by \n1.mirdha commission \n2.chelliah commision\n3.jain commision\n4.none of above\n");
-            label3:
-             choice=chose();
-             if(choice==1||choice==2||choice==3||choice==4)
-             {
-                 if(3==choice)
-                 {
-                     printf("\n correct answer, Goning to next question\n");
-                     question10();
-                 }
-                 else
-                 {
-                      printf("answer is wrong you won 25,00,000rs\n");
-                     //end()
-                 }
-             }
-             else
-             {
-                 printf("give proper input....\n");
-                 __fpurge(stdin);
-                 goto label3;
-             }
-         break;
     }
 
     
